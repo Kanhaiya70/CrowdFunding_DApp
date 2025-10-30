174def6cd8cb0de8ea1ccd246c87cf6dd64f3124
Kanhaiya70 <Kanhaiyaag964@gmail.com>
2025-10-30 19:34:54 +0530

    Create Sidebar and Navbar

---
+++ client/src/main.jsx
@@ -0,0 +1,30 @@
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import { App } from "./App";
+// import { ThirdwebProvider } from "thirdweb/react";
+// import "./index.css";
+
+// createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <ThirdwebProvider>
+//       <App />
+//     </ThirdwebProvider>
+//   </React.StrictMode>
+// );
+
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <ThirdwebProvider desiredChainId = {ChainId.Goerli}>
+    <Router>
+      <App />
+    </Router>
+  </ThirdwebProvider>
+)
