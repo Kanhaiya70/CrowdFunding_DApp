c312f07814aa753ce3b21c694662700b56567830
Kanhaiya70 <Kanhaiyaag964@gmail.com>
2025-11-06 22:41:16 +0530

    Create Context and try to connect with Metamask wallet, try to deploy smart contract

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
+import { useStateContext } from '../context';
 import { CustomButton } from './';
 import { logo, menu, search, thirdweb } from '../assets';
 import { navlinks } from '../constants';
@@ -10,7 +11,8 @@ const Navbar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState('dashboard');
   const [toggleDrawer, setToggleDrawer] = useState(false);
-  const address = '0xabc';
+
+  const { connect, address } = useStateContext();
 
   return (
     <div className='flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6'>
@@ -31,7 +33,7 @@ const Navbar = () => {
             if(address)
               navigate('create-campaign')
             else
-              'connect()'
+              connect();
           }}
         />
 
@@ -45,7 +47,7 @@ const Navbar = () => {
       {/* Small screen navigation */}
           <div className='sm:hidden flex justify-between items-center relative'>
             <div className='w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
-              <img src={thirdweb} alt='user' className='w-[60%] h-[60%] object-contain'/>
+              <img src={logo} alt='user' className='w-[60%] h-[60%] object-contain'/>
             </div>
 
             <img 
@@ -88,7 +90,7 @@ const Navbar = () => {
                     if(address)
                       navigate('create-campaign')
                     else
-                      'connect()'
+                      connect();
                   }}
                 />
               </div>
---
+++ client/src/context/index.jsx
@@ -0,0 +1,63 @@
+import React, { useContext, createContext, Children } from "react";
+import { useAddress, useContract, useContractWrite, useMetamask } from '@thirdweb-dev/react';
+import { ethers } from 'ethers';
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract('0xb377cdc529742fcdc4bc95f3ab45f30041541032');
+  const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
+
+  const address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    try{
+      const data = await createCampaign([
+        address,  // owner
+        form.title, // title
+        form.description, // description
+        form.target,  
+        new Date(form.deadline).getTime(),  // deadline
+        form.image
+      ])
+      console.log("Contract call success!", data);
+    }
+    catch (error) {
+      console.log("Contract call failure!", error);
+    }
+  }
+
+  const getCampaigns = async () => {
+    const campaigns = await contract.call('getCampaigns');
+
+    const parsedCampaigns = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+      image: campaign.image,
+      pId: i
+    }));
+
+    return parsedCampaigns;
+  }
+
+  return (
+    <StateContext.Provider
+      value={{
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+        getCampaigns,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  )
+}
+
+export const useStateContext = () => useContext(StateContext);
